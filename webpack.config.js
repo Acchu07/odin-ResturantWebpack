de803bd6793521a5ab74c45155e68a679e011b53
Acchu <acchu1915@gmail.com>
2024-02-11 04:20:34 +0530

    Require Help on Image not found

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,9 @@ const path = require('path');
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
-    output:{
+    output: {
         filename: 'main.js',
-        path: path.resolve(__dirname,'dist'),
+        path: path.resolve(__dirname, 'dist'),
     },
     devServer: {
         static: './dist'
